0ad824a0d9fa6db9acdd1bc4c927d0792ece3003
Krunal "kAOS" Shinde <kaostics12321@gmail.com>
2023-12-04 21:58:32 +0530

    CORS FIXED TRY

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import * as dotenv from 'dotenv'
 import express from 'express'
 import connectDB from './db/connectDB.js'
 import router from './routes/data.routes.js'
+import cors from 'cors'
 
 dotenv.config()
 
@@ -13,6 +14,8 @@ app.get('/', (req, res) => {
   res.send('Welcome to the API. Please use -> Endpoint: /api/data')
 })
 
+app.options("*", cors({ origin: 'http://localhost:5173', optionsSuccessStatus: 200 }));
+app.use(cors({ origin: "http://localhost:5173", optionsSuccessStatus: 200 }))
 app.use('/api/data', router)
 
 const start = async () => {
